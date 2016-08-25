8e86ed769f3c703c7f3805121a430981c805a02b
softvar <varun2902@gmail.com>
2016-08-25 14:16:34 +0530

    webpack file for automation, uglifying, eslinting

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var path = require('path');
+var env = require('yargs').argv.mode;
+
+var libraryName = 'across-tabs';
+
+var plugins = [], outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
+
+var config = {
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: 'AcrossTabs',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  eslint: {
+    failOnWarning: false,
+    failOnError: false
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;

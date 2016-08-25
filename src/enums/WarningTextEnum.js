c62b72c6210d8a23f2f232f637a57578f115d264
softvar <varun2902@gmail.com>
2016-08-25 14:17:36 +0530

    add WarningText enum file

---
+++ src/enums/WarningTextEnum.js
@@ -0,0 +1,7 @@
+const WarningTextEnum = {
+  INVALID_DATA: 'Some wrong message is being sent by Parent.',
+  URL_REQUIRED: 'Url is needed for creating and opening a new window/tab.'
+};
+
+module.exports = WarningTextEnum;
+

e282526202f08e8543e6a40b3a84ce0e2e5e36f2
softvar <varun2902@gmail.com>
2016-08-26 15:33:38 +0530

    add invalid json key,value pair too

--- src/enums/WarningTextEnum.js
+++ src/enums/WarningTextEnum.js
@@ -1,4 +1,5 @@
 const WarningTextEnum = {
+  INVALID_JSON: 'Invalid JSON Object!',
   INVALID_DATA: 'Some wrong message is being sent by Parent.',
   URL_REQUIRED: 'Url is needed for creating and opening a new window/tab.'
 };

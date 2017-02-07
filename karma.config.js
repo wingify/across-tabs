fcda2eafd2d133f83ad6aa0e93aa6de1edb14935
softvar <varun2902@gmail.com>
2017-02-07 18:34:47 +0530

    add karma config file - in one shot

---
+++ karma.config.js
@@ -0,0 +1,63 @@
+var path = require('path');
+
+module.exports = function (config) {
+  config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+    // list of files / patterns to exclude
+    exclude: [],
+    browsers: [ 'PhantomJS' ],
+    files: [
+      'tests/**/*.spec.js',
+    ],
+    frameworks: [ 'jasmine' ],
+    preprocessors: {
+      'tests/**/*.spec.js': [ 'webpack' ]
+    },
+    reporters: [ 'progress', 'coverage' ],
+    coverageReporter: [{
+      type: 'html',
+      dir: 'coverage/'
+    }],
+    webpack: {
+      cache: true,
+      module: {
+        loaders: [{
+          test: /(\.js)$/,
+          loader: 'babel',
+          exclude: /(node_modules)/,
+          query: {
+            presets: ['es2015']
+          }
+        }, {
+          test: /\.spec.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          query: {
+            cacheDirectory: true,
+            presets: ['es2015']
+          },
+        }, {// transpile and instrument only testing sources with isparta
+          test: /\.js$/,
+          include: path.resolve('src/'),
+          exclude: /(src\/vendor\/)/,
+          loader: 'isparta'
+        }],
+      },
+    },
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+    browserNoActivityTimeout: 200000,
+    browserDisconnectTolerance: 5
+  });
+};
